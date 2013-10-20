c1f3c3d9ded9af1bf2c662c45ff3f88848742478
Rogério Carvalho Schneider <stockrt@gmail.com>
2013-10-20 21:10:29 -0200

    Models de Auth e Ping

---
+++ cadastro/RegistryWeb/app/modellibrary/PingInfo.java
@@ -0,0 +1,46 @@
+package modellibrary;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+/**
+ * Serializavel PingInfo. 
+ * Será visto tanto pelo Cliente Android quando pela Módulo Controlador, então 
+ * recomenda-se que fique em algum tipo de projeto-library visto pelos dois.
+ * 
+ * @author andremd
+ *
+ */
+public class PingInfo implements Serializable {
+	
+	/**
+	 * Default Java serial version UID
+	 **/
+	private static final long serialVersionUID = 1L;
+
+	/** Client UUID */
+	private UUID uuid;
+
+	/**
+	 * Constructor.
+	 **/
+	public PingInfo(UUID _id) {
+		uuid = _id;
+	}
+	
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
+	}
+	
+	/**
+     * {@inheritDoc}
+     **/
+    @Override
+    public String toString() {
+    	return "Pong!";
+	}
+}
